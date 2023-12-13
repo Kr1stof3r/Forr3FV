914db16acd4bf3e4101f5c5b8a2c7e38ef52f2ab
Kristófer Helgi Antonsson <Kristoferan21@tskoli.is>
2023-12-13 13:07:21 +0000

    dynamic year

--- public/README.md
+++ public/README.md
@@ -7,4 +7,4 @@ Verkefni skrifað í Javascript React með Nivo library til að sjá um gröfin
 Hugmyndin var að hafa gagna/admin bakenda með mörgum "Tile"-um (power bi fílingur). 
 
 ### Lokaafurð
-Endaði á að hafa bara Eitt tile fyrir mannsfjölda sem sínir kort, þegar það þú ýtir að Tile-ið opnast popup þar sem kemur annað kort sem hægt er að interact-a við ef ýtt er að land kemur upp annað popup sem sýnir sögu mannsfjölda frá 1950 til 2023
+Endaði á að hafa bara Eitt tile fyrir mannsfjölda sem sínir kort, þegar það þú ýtir að Tile-ið opnast popup þar sem kemur annað kort sem hægt er að interact-a við ef ýtt er að land kemur upp annað popup sem sýnir sögu mannsfjölda frá 1950 til 2023. Gognin koma frá https://population.un.org gegnum api og eru dýnamisk fyrir utan listan yfir löndin, en þau eru ekki að breytast voða hratt.
--- src/getPop.js
+++ src/getPop.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 async function getPop(ids) {
-    const initialResponse = await axios.get("https://population.un.org/dataportalapi/api/v1/data/indicators/49/locations/"+ids+"?startYear=2023&endYear=2023&variants=4&sexes=3&pagingInHeader=false&format=json");
+    const currentYear = new Date().getFullYear();
+    const initialResponse = await axios.get("https://population.un.org/dataportalapi/api/v1/data/indicators/49/locations/"+ids+"?startYear="+currentYear+"&endYear="+currentYear+"&variants=4&sexes=3&pagingInHeader=false&format=json");
     const listi = [...initialResponse.data.data];
       const pages = initialResponse.data.pages;
 
       const promises = [];
 
       for (let i = 2; i <= pages; i++) {
-        const url = "https://population.un.org/dataportalapi/api/v1/data/indicators/49/locations/"+ids+"?startYear=2023&endYear=2023&variants=4&pageNumber="+i+"&sexes=3&pagingInHeader=false&format=json";
+        const url = "https://population.un.org/dataportalapi/api/v1/data/indicators/49/locations/"+ids+"?startYear="+currentYear+"&endYear="+currentYear+"&variants=4&pageNumber="+i+"&sexes=3&pagingInHeader=false&format=json";
         promises.push(axios.get(url));
       }
 
